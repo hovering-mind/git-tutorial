dedf9c169e9b03fce1f9c43e4a0fc7c0bae72aa3
hovering-mind <142564823+hovering-mind@users.noreply.github.com>
2023-11-17 23:26:53 +0530

    version 1

--- firstprogram.cpp
+++ firstprogram.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main()
 {
     int n=0;
+    cout<<"version 1";
     while(n<100)
     {
         n++;
     }
+    cout<<"version 2";
     cout<<"WELCOME TO C++ FAIZAN!";
     return 0;
 }
